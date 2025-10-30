7ae70e41ebd177e016b0068819751b3fb9c5951a
Harshit Mishra <harshitmishra.84162@gmail.com>
2025-10-30 23:00:37 +0530

    latest commit:

--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Movie(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.CharField(max_length=300)
+    active=models.BooleanField(default=True)
+    
+    
+    def __str__(self): #it is used to return string representation of the object,it is used where we need to display object as string,because the object by default returns memory address
+        return self.name
