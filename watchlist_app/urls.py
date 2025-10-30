d32c8519f62c0128b4f7fc811b4677ca3a7b835b
Harshit Mishra <harshitmishra.84162@gmail.com>
2025-10-30 23:39:40 +0530

    latest commit

--- watchlist_app/urls.py
+++ watchlist_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('list/',views.movie_list,name='movie-list')
+    path('list/',views.movie_list,name='movie-list'),
+    path('<int:pk>/',views.movie_detials,name='movie-detail'),
 ]
--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,14 +1,36 @@
 from django.shortcuts import render
 from .models import Movie
-
 from django.http import JsonResponse
-# Create your views here.
 
 
 
 def movie_list(request):
-    movies=Movie.objects.all()  # it is used to fetch all the movie objects from the database in the form of a queryset or list
+    '''the function movie_list takes a request object as a parameter. 
+        It retrieves all movie objects from the database using Movie.objects.all(). 
+        It then creates a dictionary containing a list of movies, where each movie is represented as a dictionary of its fields and their values. 
+        Finally, it returns this data as a JSON response using JsonResponse.'''
+
+    movies=Movie.objects.all()  
+    
     data={
         'movies':list(movies.values())
         }
-    return JsonResponse(data)  # values() method is used to convert the queryset into a list of dictionaries where each dictionary represents a movie object with its fields and their values
+    
+    return JsonResponse(data) 
+
+
+def movie_detials(request,pk): 
+    
+    '''the function movie_details takes a request object and a primary key (pk) as parameters. 
+    It retrieves a specific movie object from the database based on the provided primary key. 
+    It then creates a dictionary containing the movie'''
+
+    movie=Movie.objects.get(id=pk)  
+    
+    data={
+        'name':movie.name,  
+        'description':movie.description,
+        'active':movie.active
+    }
+    
+    return JsonResponse(data)
