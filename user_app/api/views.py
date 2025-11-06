40279d384bffa3ce5d96b3631da727e2921a8d3f
Harshit Mishra <harshitmishra.84162@gmail.com>
2025-11-06 14:14:07 +0530

    latest commit

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -2,7 +2,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import RegistrationSerializer
-
+from rest_framework.authtoken.models import Token
+from user_app import models
 
 @api_view(['POST'])
 def registeration_view(request):
@@ -10,7 +11,22 @@ def registeration_view(request):
     if request.method=='POST':
         serializer = RegistrationSerializer(data=request.data)
         
+        data={}
+        
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)   # response is responsible for sending back data to client in json format
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            account=serializer.save()
+            
+            """ creating custom response after successful registration """
+            data['response']='Registration Successful'
+            data['username']=account.username
+            data['email']=account.email
+            
+            token=Token.objects.get(user=account).key  # get the token created using signals in models.py
+            data['token']=token
+            
+            return Response(data, status=status.HTTP_201_CREATED)
+
+        else:
+            return Response(data, status=status.HTTP_400_BAD_REQUEST)
+
+
