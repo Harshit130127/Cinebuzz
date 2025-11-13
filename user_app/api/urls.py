9860f8bf04b7422dd0e6897ce092509175cb2e15
Harshit Mishra <harshitmishra.84162@gmail.com>
2025-11-13 11:01:19 +0530

    latest commit

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -2,8 +2,21 @@ from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path
 from . import views
 
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
+
+
 urlpatterns = [
-    path('login/', obtain_auth_token, name='login'),
+    # path('login/', obtain_auth_token, name='login'),
     path('register/', views.registeration_view, name='register'),
-    path('logout/', views.logout_view, name='logout')
+    path('logout/', views.logout_view, name='logout'),
+    
+    path('api/token/',TokenObtainPairView.as_view(), name='token_obtain_pair'),
+
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
