09651b77bb21ec3d88f5c3f5d9d4a55e2ab378d3
Harshit Mishra <harshitmishra.84162@gmail.com>
2025-11-19 13:17:45 +0530

    latest commit

--- user_app/tests.py
+++ user_app/tests.py
@@ -23,3 +23,36 @@ class RegisterTestCase(APITestCase):
         response = self.client.post(url, data, format='json')
         
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)  # comparing expected status code with actual status code
+        
+        
+        
+class LoginLogoutTestCase(APITestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser',password='testfordrf')
+        
+        
+        
+    def test_login(self):
+        url = reverse('login')
+        data = {
+            'username': 'testuser',
+            'password': 'testfordrf'
+            }
+        
+        response=self.client.post(url, data, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    
+    def test_logout(self):
+        url=reverse('logout')
+        
+        self.token=Token.objects.get(user__username='testuser')  # creating a token for the user to test logout
+        
+        
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)   # setting the token in the header for authentication
+        
+        response=self.client.get(url)
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
