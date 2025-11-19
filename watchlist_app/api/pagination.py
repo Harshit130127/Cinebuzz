fee335a9e9d953fde925de348545ffd252673e84
Harshit Mishra <harshitmishra.84162@gmail.com>
2025-11-19 10:30:56 +0530

    latest commit

--- watchlist_app/api/pagination.py
+++ watchlist_app/api/pagination.py
@@ -1,11 +1,18 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination,LimitOffsetPagination
 
 class WatchListPagination(PageNumberPagination):
     
     
     page_size=5
-    page_query_param='p' # use of this is that instead of ?page=2 we can use ?p=2
+    # page_query_param='p' # use of this is that instead of ?page=2 we can use ?p=2
     max_page_size=10 # if user tries to set a very high page size, it will be capped at this value
     page_size_query_param='size' # to allow user to set the page size dynamically
-    last_user_page_strings='end'  # to access the last page using ?page=end
+    # last_user_page_strings='last'  # to access the last page using ?page=end
      
+     
+class WatchListLOPagination(LimitOffsetPagination):
+    
+    default_limit=5
+    max_limit=10
+    limit_query_param='limit'  # to allow user to set the limit dynamically
+    offset_query_param='offset'  # to allow user to set the offset dynamically
--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -14,7 +14,7 @@ from rest_framework.throttling import UserRateThrottle,AnonRateThrottle,ScopedRa
 from watchlist_app.api.throttling import ReviewCreateThrottle, ReviewListThrottle
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
-from watchlist_app.api.pagination import WatchListPagination
+from watchlist_app.api.pagination import WatchListPagination, WatchListLOPagination
 
 
 
@@ -27,7 +27,7 @@ class WatchListGV(generics.ListCreateAPIView):
     queryset = WatchList.objects.all()
     serializer_class =  WatchListSerializer # it gives the form-like structure of data
     
-    pagination_class = WatchListPagination  # custom pagination class
+    pagination_class = WatchListLOPagination  # custom pagination class
     
     #filter_backends = [DjangoFilterBackend]
     #fileterset_fields = ['platform__name']  # filtering based on platform name
