d931cb0241f283c70f331d59292bf28eaddbac7d
Harshit Mishra <harshitmishra.84162@gmail.com>
2025-11-20 13:39:06 +0530

    latest commit

--- watchlist_app/tests.py
+++ watchlist_app/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework import status
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase  # it is used to create test cases
+from rest_framework.test import APIClient  # it is used to make requests in tests
+
+
+class StreamPlatformTestCase(APITestCase):
+    
+    def test_stream_platform_create(self):
+        
+        url=reverse('streamplatform-list')  # getting the url for stream platform list/create view
+        data={
+            'name':'Netflix',
+            'about':'Streaming Platform',
+            'website':'https://www.netflix.com'
+        }
+        
+        response=self.client.post(url, data, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
