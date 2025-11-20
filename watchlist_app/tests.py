537fc245463f3591087b282b04233be9470e9ea7
Harshit Mishra <harshitmishra.84162@gmail.com>
2025-11-20 14:11:13 +0530

    latest commit

--- watchlist_app/tests.py
+++ watchlist_app/tests.py
@@ -6,9 +6,19 @@ from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase  # it is used to create test cases
 from rest_framework.test import APIClient  # it is used to make requests in tests
-
+from watchlist_app import models
 
 class StreamPlatformTestCase(APITestCase):
+    
+    
+    def setUp(self):
+        self.user=User.objects.create_user(username='testuser', password='testpassword')
+        self.token=Token.objects.get(user=self.user)
+        self.client=APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+    
+        self.stream=models.StreamPlatform.objects.create(
+            name='Netflix', website='https://www.netflix.com', about='Streaming Platform')
     
     def test_stream_platform_create(self):
         
@@ -21,4 +31,17 @@ class StreamPlatformTestCase(APITestCase):
         
         response=self.client.post(url, data, format='json')
         
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN) # only admin can create stream platform items
+        
+        
+        
+    def test_stream_platform_list(self):
+        
+        response=self.client.get(reverse('streamplatform-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        
+    def test_stream_platform_detail(self):
+        
+        response=self.client.get(reverse('streamplatform-detail', args=(self.stream.id,)))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
